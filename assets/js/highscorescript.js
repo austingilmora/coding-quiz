f3e1e39fc179cacc27f6c34f9160caac7cda9e19
Austin Gilmore <austingilmora@gmail.com>
2021-11-06 10:39:43 -0400

    added sorting capability to the highscores

--- assets/js/highscorescript.js
+++ assets/js/highscorescript.js
@@ -2,9 +2,16 @@ var restartButton = window.document.querySelector(".restart");
 var scoreList = window.document.querySelector(".scorecard");
 
 function populateScore() {
+    // sort the scores
+    scores = scores.sort(function(a, b){return b.score - a.score});
+
     for (var i = 0; i < scores.length; i++) {
+        // make a list item
         var row = document.createElement("li");
+        
+        // put the scores objects in list items 
         row.innerHTML = "<p>Name: " + scores[i].nameInput + " Score: " + scores[i].score; + "</p>"
+    
         scoreList.appendChild(row);
     }
 }
